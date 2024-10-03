3f2f0fe694f7d88911ea291ee06cdbf0c763c1d8
Sarycheva Daria <d.saricheva@gmail.com>
2024-10-03 13:36:10 +0300

    added CianTest

---
+++ src/test/java/tests/CianTest.java
@@ -0,0 +1,70 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import tests.data.City;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static tests.data.City.Спб;
+
+public class CianTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://spb.cian.ru";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @DisplayName("Выдача по ЖК не пустая")
+
+    @ValueSource(strings = {
+            "ЖК «ALPEN»",
+            "ЖК «Дом Хороших квартир»"
+    })
+    @ParameterizedTest(name = "Для поискового запроса {0} должен отдавать не пустой список ЖК")
+    @Tag("BLOCKER")
+    void searchResultsShouldNotBeEmpty(String searchQuery) {
+        open("/");
+        $("#geo-suggest-input").setValue(searchQuery);
+        $("[data-group = 'newbuildings']").click();
+        $("[data-mark = 'FiltersSearchButton']").click();
+        $$("._93444fe79c--wrapper--W0WqH")
+                .shouldBe(sizeGreaterThan(0));
+    }
+
+    @DisplayName("Соответствие ЖК и метро")
+
+    @CsvFileSource(resources = "/test_data/searchResultsShouldContainExpectedLocation.csv")
+    @ParameterizedTest(name = "Для поискового запроса {0} в первой карточке должна быть локация {1}")
+    @Tag("SMOKE")
+    void searchResultsShouldContainExpectedLocation(String searchQuery,String expectedLocation) {
+        open("/");
+        $("#geo-suggest-input").setValue(searchQuery);
+        $("[data-name = 'SuggestionPopup']").click();
+        $("[data-mark = 'FiltersSearchButton']").click();
+        $("[data-name = 'Underground']")
+                .shouldHave(text(expectedLocation));
+    }
+
+    @DisplayName("Отображение городов в списке")
+
+    @EnumSource(City.class)
+    @ParameterizedTest(name = "Проверка городов {0}")
+    @Tag("SMOKE")
+
+    void checkApartmentsWithEnum(City city) {
+        open("/");
+        $("._025a50318d--text--SCFDt").click();
+        $("[data-name = 'GeoSwitcherBody']")
+                .shouldHave(text(Спб.description));
+    }
+}
---
+++ src/test/java/tests/data/City.java
@@ -0,0 +1,14 @@
+package tests.data;
+
+public enum City {
+    Спб("Санкт-Петербург"),
+    Мск("Москва"),
+    Нск("Новосибирск");
+
+
+public final String description;
+
+City(String description) {
+    this.description = description;
+  }
+}
